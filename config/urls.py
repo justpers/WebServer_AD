5bdcf90f9fc8959b220e1541e988390777c8a4b4
[justpers] <[nunajiyo@naver.com]>
2025-06-15 19:36:17 +0900

    feat(markdown): 마크다운 적용

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from pybo.views import base_views
 
@@ -25,3 +27,7 @@ urlpatterns = [
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
     path('api/', include('api.urls')),
 ]
+
+# 이미지 URL serving 설정
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
