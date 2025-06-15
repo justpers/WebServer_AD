b49153a219b083d8ca29950cb8566426dcd015b1
[justpers] <[nunajiyo@naver.com]>
2025-06-15 20:22:27 +0900

    feat(history): display question edit history

--- pybo/models.py
+++ pybo/models.py
@@ -30,3 +30,9 @@ class Comment(models.Model):
     modify_date = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
+class QuestionHistory(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='histories')
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    modified_at = models.DateTimeField(auto_now_add=True)
